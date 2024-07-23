16d501fe888a90c1b2ac69092b6f1741d79c47c4
leaazar <lea.g.azar1@gmail.com>
2024-07-23 10:47:33 +0300

    Add .gitignore file to ignore virtual environment

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,48 @@
-from Flask import Flask 
-app = Flask(__name__) 
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
 
-# routing the decorator function hello_name 
-@app.route('/hello/<name>') 
-def hello_name(name): 
-    return 'I love you %s!' % name 
+app = Flask(__name__)
 
-if __name__ == '__main__': 
-    app.run(debug = True) 
+# Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'lea'
+app.config['MYSQL_PASSWORD'] = 'Lea.gilbert1'
+app.config['MYSQL_DB'] = 'todo_db'
+
+mysql = MySQL(app)
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM tasks')
+    tasks = cur.fetchall()
+    cur.close()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_task():
+    if request.method == 'POST':
+        name_task = request.form.get('name_task')
+        deadline = request.form.get('deadline')
+        print(f"name_task: {name_task}, deadline: {deadline}")  # Debugging
+        if name_task and deadline:
+            cur = mysql.connection.cursor()
+            cur.execute('INSERT INTO tasks (name_task, deadline) VALUES (%s, %s)', (name_task, deadline))
+            mysql.connection.commit()
+            cur.close()
+            return redirect(url_for('index'))
+    return render_template('add_task.html')
+
+
+
+@app.route('/remove', methods=['POST'])
+def remove_task():
+    task_name = request.form['task_name']
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM tasks WHERE name_task = %s', (task_name,))
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
